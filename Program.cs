ea482a380812358524ed7668d639eaa1f9549401
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-19 21:49:16 +0300

    Ex 10 solved

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Write ("Введит трехзначное число: ");
+int num=int.Parse(Console.ReadLine());
+int secNum=num/10%10;
+Console.WriteLine ($"Вторая цифра числа {num} = {secNum}");
+
+
